7e833072eb17e0c9e7db2867dd56a1a8ff0548f0
diego <ddsgonas@gmail.com>
2026-06-10 19:50:10 -0500

    arreglos para dev

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -3,5 +3,8 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 import { config } from '../lib/config'
 import * as schema from './schema'
 
-const pool = new Pool({ connectionString: config.databaseUrl })
+const pool = new Pool({
+  connectionString: config.databaseUrl,
+  ssl: { rejectUnauthorized: false },
+})
 export const db = drizzle(pool, { schema })
